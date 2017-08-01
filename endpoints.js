823f2a04ef60a3290fd8b9f39656367d6877ccd2
John Goff <gygn.johngoff@gmail.com>
2017-08-01 17:18:51 -0400

    Store cookie locally and display the username

--- endpoints.js
+++ endpoints.js
@@ -4,26 +4,36 @@ module.exports = function (app, database) {
   app.get("/", (req, res) => {
     res.end("Hello!");
   });
-  
-  app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
 
+  app.post("/login", (req, res) => {
+    res.cookie("username", req.body.username);
+    res.redirect(303, "/urls");
+  });
+  
   app.post("/urls", (req, res) => {
     let newID = random();
     database[newID] = req.body.longURL;
-    res.redirect(302, `/urls/${newID}`);
+    res.redirect(303, `/urls/${newID}`);
   });
 
   app.get("/urls", (req, res) => {
     let templateVars = {
+      username: req.cookies.username,
       urls: database,
     };
     res.render("urls_index", templateVars);
   });
 
+  app.get("/urls/new", (req, res) => {
+    let templateVars = {
+      username: req.cookies.username,
+    }
+    res.render("urls_new", templateVars);
+  });
+
   app.get("/urls/:id", (req, res) => {
     let templateVars = {
+      username: req.cookies.username,
       shortURL: req.params.id,
       fullURL: database[req.params.id],
     };
@@ -32,12 +42,12 @@ module.exports = function (app, database) {
 
   app.post("/urls/:id", (req, res) => {
     database[req.params.id] = req.body.newURL;
-    res.redirect(302, "/urls");
+    res.redirect(303, "/urls");
   });
 
   app.post("/urls/:id/delete", (req, res) => {
     delete database[req.params.id];
-    res.redirect(302, "/urls");
+    res.redirect(303, "/urls");
   });
 
   app.get("/urls.json", (req, res) => {
@@ -49,7 +59,7 @@ module.exports = function (app, database) {
     if (!longURL) {
       res.status(404).send(`ERROR 404: No url ${req.params.shortURL} currently exists on server. Click <a href="/urls">here</a> to go to url page.`);
     }
-    res.redirect(302, longURL);
+    res.redirect(301, longURL);
   });
 
   app.get("/hello", (req, res) => {
--- express-server.js
+++ express-server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,6 +12,7 @@ const urlDatabase = {
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 require("./endpoints")(app, urlDatabase);
 
